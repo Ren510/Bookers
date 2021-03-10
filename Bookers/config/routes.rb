a1fe69f7ece89658edce430a060e89085fa37b2a
Ren510 <yt.1141977@gmail.com>
2021-03-10 08:17:16 +0000

    [Start]kadai2

--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -1,19 +1,4 @@
 Rails.application.routes.draw do
-  root 'books#index'
-
-  root 'books#show'
-
-  root 'books#new'
-
-  root 'books#edit'
-
-  root 'homes#index'
-
-  root 'homes#show'
-
-  root 'homes#new'
-
-  root 'homes#edit'
-
+root 'homes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
