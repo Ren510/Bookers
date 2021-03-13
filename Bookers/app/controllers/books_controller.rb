79bbc53192f1e418c77d61be25aca30312a9e36f
Ren510 <yt.1141977@gmail.com>
2021-03-13 15:03:10 +0000

    kadai2

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -14,17 +14,21 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+  if  book.save
+    flash.now[:notice] = "登録に失敗しました"
+    redirect_to books_path
+  else  
+      render :new
   end
+end
 
   def edit
     @book = Book.find(params[:id])
   end
   
   def update
-    @book = Book.find(params[:id])
-    @book.update(book_params)
+    book = Book.find(params[:id])
+    book.update(book_params)
     redirect_to book_path(book.id)
   end
   
@@ -40,5 +44,3 @@ class BooksController < ApplicationController
   end
   
 end
-
-
