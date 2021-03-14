694356321777a2bfa70bfe4ae94bd32abd97e40e
Ren510 <yt.1141977@gmail.com>
2021-03-14 08:58:53 +0000

    [kadai2]30

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -11,36 +11,43 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
-    book = Book.new(book_params)
-  if  book.save
-    flash.now[:notice] = "登録に失敗しました"
-    redirect_to books_path
-  else  
-      render :new
+    @book = Book.new(book_params)
+  if  @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book)
+  else
+    @books = Book.all
+      render :index
   end
 end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+  if  @book.save
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+  else
+    @books = Book.all
+      render :edit
   end
-  
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title,:body)
   end
-  
+
 end
