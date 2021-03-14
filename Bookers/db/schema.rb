526487324e2bd0070ba2f160c74ade1608398aef
Ren510 <yt.1141977@gmail.com>
2021-03-14 10:26:23 +0000

    [kadai]clear

--- Bookers/db/schema.rb
+++ Bookers/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210309045849) do
+ActiveRecord::Schema.define(version: 2021_03_09_045849) do
 
   create_table "books", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
